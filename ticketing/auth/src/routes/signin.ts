0c16482452e600aa8ec7303c3e71c7def5bceb5d
dmnhat91 <henry.duong02@gmail.com>
2022-03-29 21:24:55 +0700

    Format files

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!')
+    res.send('Hi there!');
 });
 
 export { router as signinRouter}; //rename because we have many routers (they can't be the same name)
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signup', (req, res) => {
-    res.send('Hi there!')
+    res.send('Hi there!');
 });
 
 export { router as signupRouter}; //rename because we have many routers (they can't be the same name)
