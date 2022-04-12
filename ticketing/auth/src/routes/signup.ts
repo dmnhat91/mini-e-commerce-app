5d5d5e61da5631c266975dfd8f46b94bdc9b4636
dmnhat91 <henry.duong02@gmail.com>
2022-04-12 15:29:28 +0700

    Create User on MongoDb

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import { User } from '../models/user';
 import {RequestValidationError} from '../errors/request-validation-error';
-import {DatabaseConnectionError} from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -16,7 +16,7 @@ router.post('/api/users/signup', [
         .withMessage("Password must be between 4 and 20 characters")
 
 ], 
-(req: Request, res: Response) => {
+async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -25,11 +25,17 @@ router.post('/api/users/signup', [
 
     const {email, password} = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({email});
 
-    res.send({});
-    
+    if (existingUser){
+        console.log('Email in use');
+        return res.send({});
+    }
+
+    const user = User.build({email, password});
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter}; //rename because we have many routers (they can't be the same name)
