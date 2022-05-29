d7d99cf2c0282e8c05db8fd33e7269dbd139477c
dmnhat91 <henry.duong02@gmail.com>
2022-05-29 14:21:47 +0700

    Update signout logics

--- ticketing/auth/src/routes/signout.ts
+++ ticketing/auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there!');
+    req.session = null;
+
+    res.send({});
 });
 
 export { router as signoutRouter}; //rename because we have many routers (they can't be the same name)
