444bba5ceb08c825ffc20332bd8dfea5fdefeea3
dmnhat91 <henry.duong02@gmail.com>
2022-06-14 21:30:09 +0700

    Refactor auth index.ts

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import {json} from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import {currentUserRouter} from './routes/current-user';
-import {signinRouter} from './routes/signin';
-import {signoutRouter} from './routes/signout';
-import {signupRouter} from './routes/signup';
-import {errorHandler} from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true); //traffic is directed through Ingress nginx, by default, express will think like 'hey there is a proxy here, we don't trust this https connection'. So we do this so that the app is aware it is behind the proxy of Ingress nginx and trust the proxy being secure.
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false, //disable cookie encryption
-        secure: true
-    }) 
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import {app} from './app';
 
 const start = async () => {
     if (!process.env.JWT_KEY){
