95d51611bd429ddca84f055a7f15f16cb17f9048
dmnhat91 <henry.duong02@gmail.com>
2022-06-15 21:40:49 +0700

    Refactor currentuser test
    Write global function so we can easily use it for every test file. An alternative way is to create a new helper file, export it then use 'import'.

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -2,7 +2,12 @@
 //without them all trying to raech out the same copy of Mongo. Mongo memory server also gives us direct memory access to the small DB database.
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import {app} from '../app';
+
+declare global {
+	var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 //running beforeALl codes before running the tests
@@ -29,3 +34,21 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 })
+
+//create global scope to reuse for every test files
+global.signin = async () => {
+    //sign up first
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email, password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
