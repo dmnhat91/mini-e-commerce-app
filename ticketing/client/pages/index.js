8f01a89f73eeec376f3cefb9a8b6095a5dfa9c92
dmnhat91 <henry.duong02@gmail.com>
2022-06-16 22:36:38 +0700

    Include Cookie for currentuser from UI

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -6,16 +6,16 @@ const LandingPage = ({currentUser}) => {
     return <h1>Landing Page</h1>;
  };
  
- LandingPage.getInitialProps = async () => {
+ LandingPage.getInitialProps = async ({req}) => {
+    // console.log(req.headers);
+
     if (typeof window === 'undefined'){ 
         //if window variable undefined, meaning we are on server
         //requests should be made to ingress nginx
         const {data} = await axios.get(
             'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
             {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
+                headers: req.headers
             }
         );
 
@@ -23,7 +23,7 @@ const LandingPage = ({currentUser}) => {
     } else {
         //we are on the browser
         //requests can be made with a base url of ''
-        const {data} = await axios.get('/api/users/currentuser');
+        const {data} = await axios.get('/api/users/currentuser'); //we don't need to attach headers as browser does it for us automatically
 
         return data;
     }
