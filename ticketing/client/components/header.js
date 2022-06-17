29b9d09e02af9cd61813884a4d0793c65ddc3b46
dmnhat91 <henry.duong02@gmail.com>
2022-06-17 21:29:28 +0700

    Display links

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,6 +1,22 @@
 import Link from 'next/link';
 
 export default ({currentUser}) => {
+    const links = [
+        !currentUser && {label: 'Sign Up', href: '/auth/signup'}, //if user NOT signed in, then Sign Up is defined inside the links array
+        !currentUser && {label: 'Sign In', href: '/auth/signin'},
+        currentUser && {label: 'Sign Out', href: '/auth/signout'}
+    ] //  will be either [false, false, {currentUser && {label: 'Sign Out', href: '/auth/signout'}] OR [{..},{..}, false]
+        .filter(linkConfig => linkConfig) //filter out 'false' entries
+        .map(({label, href}) => { //destructure label, href of the object
+            return (
+            <li key={href} className="nav-item">
+                <Link href={href}>
+                    <a className="nav-link">{label}</a>
+                </Link>
+            </li>
+            );
+        });
+
     return (
     <nav className="navbar navbar-light bg-light">
         <Link href="/">
@@ -9,7 +25,7 @@ export default ({currentUser}) => {
 
         <div className="d-flex justify-content-end">
             <ul className="nav d-flex align-items-center">
-                {currentUser ? 'Sign out' : 'Sign in/up'}
+                {links}
             </ul>
         </div>
     </nav>
